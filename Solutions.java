798b12219640459c37807610b4692d702c39be5d
Keaton Stwalley <23keatonstwalley@usd266.com>
2023-01-31 12:05:24 -0600

    added max3

--- Solutions.java
+++ Solutions.java
@@ -1,5 +1,42 @@
 public class Solutions {
+    
+    public static int max3(int a, int b, int c){
+        int [] numList = new int[3];
+        numList[0] = a;
+        numList[1] = b;
+        numList[2] = c;
+        int maxvalue = numList[0];
+        for (int i = 0; i<numList.length;i++){
+            if (maxvalue < numList[i]){
+                maxvalue = numList[i];
+            }
+        }
+        return(maxvalue);
+    }
+
+    public static double max3(double a, double b, double c){
+        double [] numList = new double[3];
+        numList[0] = a;
+        numList[1] = b;
+        numList[2] = c;
+        double maxvalue = numList[0];
+        for (int i = 0; i<numList.length;i++){
+            if (maxvalue < numList[i]){
+                maxvalue = numList[i];
+            }
+        }
+        return(maxvalue);
+    }
+
+
+    
+    
     public static void main(String[] args) {
-        
+        System.out.println(max3(2,-5,3));
+        System.out.println(max3(2.0,5.3,5.3000001));
     }
 }
+
+
+
+//javac Solutions.java; java Solutions
